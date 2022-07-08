8a0c9801dee15bc462c49453a54d36c55f14706c
luisdergal <luisandresdergal@gmail.com>
2022-07-08 04:22:19 -0500

    iconos a navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Cartwidget from '../CartWidget/Cartwidget'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faAlignJustify} from '@fortawesome/free-solid-svg-icons'
+import {faMicrochip} from '@fortawesome/free-solid-svg-icons'
+import {faMemory} from '@fortawesome/free-solid-svg-icons'
+import {faDesktop} from '@fortawesome/free-solid-svg-icons'
+import {faKeyboard} from '@fortawesome/free-solid-svg-icons'
 
 const NavBar = () => {
   return (
@@ -16,33 +19,43 @@ const NavBar = () => {
                 <ul className="navbar-nav m-auto mb-2 mb-lg-0 navLinks">
                     <li className="nav-item me-3">
                     <a className="nav-link" href="#">Tarjetas gráficas</a>
+                    <img className='iconos' src="../../../assets/images/tarjeta-grafica.svg" alt="" />
                     </li>
                     <li className="nav-item me-3">
                     <a className="nav-link" href="#">Procesadores</a>
+                    <span className='iconosFa'><FontAwesomeIcon icon={faMicrochip}/></span>
                     </li>
                     <li className="nav-item me-3">
                     <a className="nav-link" href="#">Motherboards</a>
+                    <img className='iconos' src="../../../assets/images/motherboard.svg" alt="" />
                     </li>
                     <li className="nav-item me-3">
                     <a className="nav-link" href="#">Fuentes de poder</a>
+                    <img className='iconos' src="../../../assets/images/power-supply.svg" alt="" />
                     </li>
                     <li className="nav-item me-3">
                     <a className="nav-link" href="#">Gabinetes</a>
+                    <img className='iconos' src="../../../assets/images/tower-pc.svg" alt="" />
                     </li>
                     <li className="nav-item me-3">
                     <a className="nav-link" href="#">Memoria Ram</a>
+                    <span className='iconosFa'><FontAwesomeIcon icon={faMemory}/></span>
                     </li>
                     <li className="nav-item me-3">
                     <a className="nav-link" href="#">Almacenamiento</a>
+                    <img className='iconos' src="../../../assets/images/ssd-drive.svg" alt="" />
                     </li>
                     <li className="nav-item me-3">
                     <a className="nav-link" href="#">Monitores</a>
+                    <span className='iconosFa'><FontAwesomeIcon icon={faDesktop}/></span>
                     </li>
                     <li className="nav-item me-3">
                     <a className="nav-link" href="#">Refrigeración</a>
+                    <img className='iconos' src="../../../assets/images/cooler.svg" alt="" />
                     </li>
                     <li className="nav-item me-3">
                     <a className="nav-link" href="#">Periféricos</a>
+                    <span className='iconosFa'><FontAwesomeIcon icon={faKeyboard}/></span>
                     </li>
                 </ul>
                 </div>
