ae25b56e8fc62e8ad288c08529a67e0bc2202a45
luisdergal <luisandresdergal@gmail.com>
2022-07-08 03:28:18 -0500

    implementacion boostrap

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,24 +5,53 @@ import {faAlignJustify} from '@fortawesome/free-solid-svg-icons'
 
 const NavBar = () => {
   return (
-    <header className='header'>
-        <div className='logo'>
-            <a href=""><img src="../../../assets/images/losGamaLogo.svg" alt="" /></a>
-        </div>
-        <nav>
-            <ul className='navLinks'>
-                <li><a href="">Tarjetas graficas</a></li>
-                <li><a href="">Procesadores</a></li>
-                <li><a href="">Motherboards</a></li>
-                <li><a href="">Fuentes de poder</a></li>
-                <li><a href="">Gabinetes</a></li>
-                <li><a href="">Memoria Ram</a></li>
-                <li><a href="">Almacenamiento</a></li>
-                <li><a href="">Monitores</a></li>
-                <li><a href="">Periféricos</a></li>
-            </ul>
+    <header>
+        <nav className="navbar navbar-expand-xxl navbar-custom">
+            <div className="container-fluid">
+                <a className="navbar-brand ms-4" id='containerLogo' href="#"><img src="../../../assets/images/losGamaLogo.svg" id='logoGU' alt="LogoMarca" /></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarText">
+                <ul className="navbar-nav m-auto mb-2 mb-lg-0 navLinks">
+                    <li className="nav-item me-3">
+                    <a className="nav-link" href="#">Tarjetas gráficas</a>
+                    </li>
+                    <li className="nav-item me-3">
+                    <a className="nav-link" href="#">Procesadores</a>
+                    </li>
+                    <li className="nav-item me-3">
+                    <a className="nav-link" href="#">Motherboards</a>
+                    </li>
+                    <li className="nav-item me-3">
+                    <a className="nav-link" href="#">Fuentes de poder</a>
+                    </li>
+                    <li className="nav-item me-3">
+                    <a className="nav-link" href="#">Gabinetes</a>
+                    </li>
+                    <li className="nav-item me-3">
+                    <a className="nav-link" href="#">Memoria Ram</a>
+                    </li>
+                    <li className="nav-item me-3">
+                    <a className="nav-link" href="#">Almacenamiento</a>
+                    </li>
+                    <li className="nav-item me-3">
+                    <a className="nav-link" href="#">Monitores</a>
+                    </li>
+                    <li className="nav-item me-3">
+                    <a className="nav-link" href="#">Refrigeración</a>
+                    </li>
+                    <li className="nav-item me-3">
+                    <a className="nav-link" href="#">Periféricos</a>
+                    </li>
+                </ul>
+                </div>
+                <div className='me-4'>
+                    <Cartwidget></Cartwidget>
+                </div>
+            </div>
+
         </nav>
-        <Cartwidget></Cartwidget>
     </header>
   )
 }
