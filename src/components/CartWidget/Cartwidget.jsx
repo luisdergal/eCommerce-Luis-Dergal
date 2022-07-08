1f2ac40fe6d430d54fa3518e0d586d6b4c373dfe
luisdergal <luisandresdergal@gmail.com>
2022-07-08 03:27:16 -0500

    implementacion bootstrap

--- src/components/CartWidget/Cartwidget.jsx
+++ src/components/CartWidget/Cartwidget.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Cartwidget = () => {
   return (
-      <a className='carrito' href="">
+      <a className='carrito'  href="">
         <img className='iconoCart' src="../../../assets/images/carritoCompras.svg" alt="" />
         <p>1</p>
       </a>   
