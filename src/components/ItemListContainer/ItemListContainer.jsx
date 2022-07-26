c3c0a4e63f33e2ac81c62cda266dcc7e27a1eedf
luisdergal <luisandresdergal@gmail.com>
2022-07-25 21:15:55 -0500

    ...

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,18 +1,77 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { getFetch } from '../../helpers/getFetch'
 import ItemCount from '../ItemCount/ItemCount'
 import ItemList from '../Items/ItemList'
 
-const ItemListContainer = (obj) => {
 
-  const onAdd = (cantidad) => {
-    console.log(` La cantidad es: ${cantidad}`)
-  }  
+const ItemListContainer = () => {
+  const [ productos, setProductos ] = useState([])  
+  const [ loading, setLoading ] = useState(true)
+
+  const {categoriaId} = useParams()
+
+  useEffect(()=>{
+    if(categoriaId){
+        getFetch() 
+        .then(respuesta => setProductos(respuesta.filter(producto => producto.categoria === categoriaId)))    
+        .catch( err => console.log(err) )
+        .finally(()=> setLoading(false) )
+    }else{
+        getFetch() 
+        .then(respuesta => setProductos(respuesta))    
+        .catch( err => console.log(err) )
+        .finally(()=> setLoading(false) )
+    }
+  }, [categoriaId])
+
+  const onAdd = (cant) => {
+    console.log(`La cantidad es:  ${cant}`)
+  }
+
   return (
     <div>
-        <ItemList></ItemList>
+
+      {loading ?
+      <div className='spinner container'></div>
+      :
+      <ItemList productos={productos}/>
+      }
+
+
     </div>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const ItemListContainer = (obj) => {
+
+//   const onAdd = (cantidad) => {
+//     console.log(` La cantidad es: ${cantidad}`)
+//   }  
+//   return (
+//     <div>
+//         <ItemList></ItemList>
+//     </div>
+//   )
+// }
 
 export default ItemListContainer
