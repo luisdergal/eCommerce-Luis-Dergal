9a7474fca402899bb080e859ac53598fc106450d
luisdergal <luisandresdergal@gmail.com>
2022-07-26 05:42:57 -0500

    detalles card bootstrap

--- src/components/Items/ItemDetail.jsx
+++ src/components/Items/ItemDetail.jsx
@@ -3,16 +3,21 @@ import ItemCount from '../ItemCount/ItemCount'
 
 const ItemDetail = ({producto}) => {
   return (
-    <div className='row'>
-        <div className="col">
+    <div className='d-flex justify-content-around'>
+        <div>
             <img className='mt-5' src={producto.img} alt="" />
         </div>
-        <div className="col">
-            <div className='mt-5'>
-                <h1>{producto.nombre}</h1>
+        <div className='card text-center mt-5'>
+            <div className='card-header'>
+                <h1 className='fs-6'>{producto.categoria}</h1>
             </div>
-            <div className='detailCount'>
-                <ItemCount/>
+            <div className='card-body row'>
+                <h5 className='card-title fs-1 mt-5'>{producto.nombre}</h5>
+                <h4>Precio: {producto.precio} $</h4>
+                <ItemCount></ItemCount>
+            </div>
+            <div className='card-footer text-muted'>
+                stock: {producto.stock}
             </div>
         </div>
     </div>
