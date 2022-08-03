a02af2fa8d37446fda3874eb00f76aaa7dddf0a5
luisdergal <luisandresdergal@gmail.com>
2022-08-02 23:44:17 -0500

    Noroco

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,7 +4,8 @@ const Cart = () => {
   return (
     <div>Cart Page
       <div>
-        <img src="http://www.fsconcreto.com/reyluminar.png" alt="building" />
+        {/* <img src="http://www.fsconcreto.com/reyluminar.png" alt="building" /> */}
+        <img src="../../assets/images/noroquito.png" alt="" />
         <h1>Sitio en construcción.</h1>
       </div>
     </div>
