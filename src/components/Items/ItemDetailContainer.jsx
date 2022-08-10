6734398e655f024a4a4043280ade38e10cd94739
luisdergal <luisandresdergal@gmail.com>
2022-08-10 03:48:47 -0500

    migracion a firebase

--- src/components/Items/ItemDetailContainer.jsx
+++ src/components/Items/ItemDetailContainer.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { getFetch } from '../../helpers/getFetch'
 import ItemDetail from './ItemDetail'
+import { getFirestore, doc, getDoc } from 'firebase/firestore'
 
 const ItemDetailContainer = () => {
 
@@ -12,9 +13,13 @@ const ItemDetailContainer = () => {
 
 
     useEffect(()=>{
-        getFetch(detailId)
-        .then(resp => setProducto(resp))
-    }, [])
+      const db = getFirestore()
+      const queryProducto = doc(db, 'items', detailId)
+      getDoc(queryProducto)
+      .then(resp => setProducto( { id: resp.id, ...resp.data() } ) )
+      .catch( err => console.log(err) )
+      .finally( () => setLoading(false) )
+  }, [])
 
   return (
     <div>
