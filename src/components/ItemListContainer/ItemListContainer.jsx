214fb6e8a63b567f5af64a87906c6516a57c1746
luisdergal <luisandresdergal@gmail.com>
2022-08-10 03:47:47 -0500

    imigracion a firebase

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { collection, doc, getDoc, getDocs, getFirestore} from 'firebase/firestore'
 import { getFetch } from '../../helpers/getFetch'
 import ItemCount from '../ItemCount/ItemCount'
 import ItemList from '../Items/ItemList'
@@ -7,22 +8,46 @@ import ItemList from '../Items/ItemList'
 const ItemListContainer = () => {
   const [ productos, setProductos ] = useState([])  
   const [ loading, setLoading ] = useState(true)
-
   const {categoriaId} = useParams()
+  const [producto, setProducto] = useState({})  
 
-  useEffect(()=>{
-    if(categoriaId){
-        getFetch() 
-        .then(respuesta => setProductos(respuesta.filter(producto => producto.categoria === categoriaId)))    
-        .catch( err => console.log(err) )
-        .finally(()=> setLoading(false) )
-    }else{
-        getFetch()
-        .then(respuesta => setProductos(respuesta))    
-        .catch( err => console.log(err) )
-        .finally(()=> setLoading(false) )
-    }
-  }, [categoriaId])
+  // FireBase// Traer productos por id
+
+  // useEffect(() => {
+  //   const db = getFirestore()
+  //   const queryProducto = doc(db, 'items', '2hGxBqe8lT623lqFFazo')
+  //   getDoc(queryProducto)
+  //   .then(resp => setProducto({id:resp.id, ...resp.data()}))
+  // },[])
+
+  
+
+  useEffect(() => {
+    const db = getFirestore()
+    const queryCollection = collection(db, 'items')
+    getDocs(queryCollection)
+    .then(resp => setProductos(resp.docs.map(prod => ({id: prod.id, ...prod.data()}))))   
+    .catch(err => console.log(err))
+    .finally(() => setLoading(false))
+  },[])
+
+  console.log(productos)
+
+
+
+  // useEffect(()=>{
+  //   if(categoriaId){
+  //       getFetch() 
+  //       .then(respuesta => setProductos(respuesta.filter(producto => producto.categoria === categoriaId)))    
+  //       .catch( err => console.log(err) )
+  //       .finally(()=> setLoading(false) )
+  //   }else{
+  //       getFetch()
+  //       .then(respuesta => setProductos(respuesta))    
+  //       .catch( err => console.log(err) )
+  //       .finally(()=> setLoading(false) )
+  //   }
+  // }, [categoriaId])
 
 
 
