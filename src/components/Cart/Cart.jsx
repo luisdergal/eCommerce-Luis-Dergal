0f42f01902a4cd5cdc308f853653286e5f511b59
luisdergal <luisandresdergal@gmail.com>
2022-08-20 23:32:52 -0500

    carrito al 90

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,111 +1,177 @@
-import { addDoc, collection, getFirestore } from "firebase/firestore"
-import { useCartContext } from "../../Context/CartContext"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
-
-
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { useCartContext } from "../../Context/CartContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping, faFrown } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-
-  const {cartList, vaciarCarrito, eliminarProducto, precioTotal, cantidadTotal} = useCartContext()
+  const {
+    cartList,
+    vaciarCarrito,
+    eliminarProducto,
+    precioTotal,
+    cantidadTotal,
+  } = useCartContext();
 
   //Función para guardar la orden en la base de datos
 
   /// Setear la orden
 
   const guardarOrden = async (e) => {
-    e.preventDefault()
-    
-    const fecha = new Date()
-    const order = {}
-    order.buyer = {email: "luis@gmail.com", name: "Luis", phone: "123456789", date: fecha}
+    e.preventDefault();
 
-    order.items = cartList.map(prod => {
-        return {
-            product: prod.nombre,
-            id: prod.id,
-            price: prod.precio
-        }
-    })
-    
-    order.total = precioTotal()
+    const fecha = new Date();
+    const order = {};
+    order.buyer = {
+      email: "luis@gmail.com",
+      name: "Luis",
+      phone: "123456789",
+      date: fecha,
+    };
+
+    order.items = cartList.map((prod) => {
+      return {
+        product: prod.nombre,
+        id: prod.id,
+        price: prod.precio,
+      };
+    });
+
+    order.total = precioTotal();
 
     //// Guardar la orden en la base de datos
 
-
-    const db = getFirestore()
-    const queryOrders = collection(db, "orders")
+    const db = getFirestore();
+    const queryOrders = collection(db, "orders");
     addDoc(queryOrders, order)
-    .then(resp => Swal.fire('Su orden ha sido generada con el identificador: ' + resp.id + " \n Revisa tu email para el seguimiento del envío."), console.log(order))
-    .catch(err => console.log(err))
-    .finally(() => vaciarCarrito())  
-
-  }
-  
+      .then(
+        (resp) =>
+          Swal.fire(
+            "Su orden ha sido generada con el identificador: " +
+              resp.id +
+              " \n Revisa tu email para el seguimiento del envío."
+          ),
+        console.log(order)
+      )
+      .catch((err) => console.log(err))
+      .finally(() => vaciarCarrito());
+  };
 
   return (
-      <div className="container-md cartContainer">
+    <>
+      {cartList == 0 ? (
+        <div className="containerCarritoVacio">
+          <h1>Tu carrito está vacío. 
+          <FontAwesomeIcon icon={faFrown} />
+          </h1>
+          <Link to="/">
+                    <button className="btn btn-outline-dark btn-block mt-5">
+                        Ir al inicio.
+                    </button>
+                    </Link>
+          </div>
+            ) : (
+              <div className="container-md cartContainer">
         <div className="row">
-          <div className="col-sm"><h2 className="mt-3">Carrito <span className='iconosFa'><FontAwesomeIcon icon={faCartShopping}/></span></h2> </div>
+          <div className="col-sm">
+            <h2 className="mt-3">
+              Carrito{" "}
+              <span className="iconosFa">
+                <FontAwesomeIcon icon={faCartShopping} />
+              </span>
+            </h2>{" "}
+          </div>
         </div>
         <div className="row barraCarrito">
           <div className="col-sm">
-            <span className="fw-semibold">{ cantidadTotal() !== 0 && ` Tienes: ${ cantidadTotal() } productos en tu carrito.`}</span>
+            <span className="fw-semibold">
+              {cantidadTotal() !== 0 &&
+                ` Tienes: ${cantidadTotal()} productos en tu carrito.`}
+            </span>
           </div>
           <div className="col-sm">
-          <button className="btn btn-outline-danger mb-2 btn-sm" onClick={vaciarCarrito}> Vaciar Carrito. </button>
+            {cartList == 0 ? (
+              ""
+            ) : (
+              <button
+                className="btn btn-outline-danger mb-2 btn-sm"
+                onClick={vaciarCarrito}
+              >
+                {" "}
+                Vaciar Carrito.{" "}
+              </button>
+            )}
           </div>
         </div>
         <div className="row mt-3">
-          <div className="col-md-6"><p className="fw-semibold">Checkout de productos.</p>
+          <div className="col-md-6">
+            <p className="fw-semibold">Checkout de productos.</p>
             <div className="col checkout">
               <ul className="">
-              {cartList.map(item => (
-                <li key={item.id}>
-                  <div className="listProductos mt-5">
-                    <h3>
-                      {item.nombre}
-                    </h3>
-                  </div>
-                </li>
-              ))}
+                {cartList.map((item) => (
+                  <li key={item.id}>
+                    <div className="listProductos mt-5">
+                      <h3>{item.nombre}</h3>
+                    </div>
+                  </li>
+                ))}
               </ul>
             </div>
             <div className="col checkoutText mt-5">
-            <h5>  { precioTotal() !== 0 && `Precio Total: ${ precioTotal() } $`} </h5>
+              <h5>
+                {" "}
+                {precioTotal() !== 0 && `Precio Total: ${precioTotal()} $`}{" "}
+              </h5>
             </div>
             <div className="col checkoutText mt-5">
-              <button className="btn btn-success" onClick={guardarOrden}> Finalizar Compra </button>
+              {cartList == 0 ? (
+                ""
+              ) : (
+                <button className="btn btn-success" onClick={guardarOrden}>
+                  {" "}
+                  Finalizar Compra{" "}
+                </button>
+              )}
             </div>
           </div>
-          <div className="col-md-6"> <p className="textoCarrito fw-semibold">Productos seleccionados:</p>
-          <ul className="mt-4">
-          {cartList.map(item => (
-              <li key={item.id}>
-                <div className="card">
-                <div className="card-body">
-                  
-                  <img src={item.img} alt='' className='w-25' />
-                  <button className="btn btn-danger m-2 btn-sm" onClick={ () => eliminarProducto(item.id) }>X</button>                                                        
-                </div>
-                  <div className="card-body">
-                    <p> {item.nombre }</p>
-                    <p> Cantidad: {item.cantidad}</p>
-                    <p>Precio: {item.precio * item.cantidad} $ </p>
+          <div className="col-md-6">
+            {" "}
+            <p className="textoCarrito fw-semibold">Productos seleccionados:</p>
+            <div className="contenedorCarroCards">
+            <ul>
+              {cartList.map((item) => (
+                <li key={item.id}>
+                  <div className="card cardCarrito">
+                    <div className="card-body">
+                      <Link to={`/detail/${item.id}`}>
+                      <img src={item.img} alt="" className="w-25" />
+                      </Link>
+                      <button
+                        className="btn btn-danger m-2 btn-sm"
+                        onClick={() => eliminarProducto(item.id)}
+                      >
+                        X
+                      </button>
+                    </div>
+                    <div>
+                      <p className="p-2"> {item.nombre}</p>
+                      <p> Cantidad: {item.cantidad}</p>
+                      <p>Precio: {item.precio * item.cantidad} $ </p>
+                    </div>
                   </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+                </li>
+              ))}
+            </ul>
+            </div>
           </div>
         </div>
         <div className="row">
-          <div className="col-md-12">
-            Aceptamos Visa y Mastercard
-          </div>
+          <div className="col-md-12 mt-5">Aceptamos Visa y Mastercard</div>
         </div>
       </div>
-  )
-}
+            )}
+    </>
+  );
+};
 
-export default Cart
+export default Cart;
--- src/components/Items/ItemDetail.jsx
+++ src/components/Items/ItemDetail.jsx
@@ -23,13 +23,14 @@ const ItemDetail = ({producto}) => {
         <div>
             <img className='mt-5' src={producto.img} alt="" />
         </div>
-        <div className='card text-center mt-5'>
+        <div className='card text-center mt-5 w-50'>
             <div className='card-header'>
                 <h1 className='fs-6'>{producto.categoria}</h1>
             </div>
             <div className='card-body row'>
                 <h5 className='card-title fs-1 mt-5'>{producto.nombre}</h5>
-                <h4>Precio: {producto.precio} $</h4>
+                <h3 className='fs-4 m-1 text-body'>{producto.descripcion}</h3>
+                <h4 className='fs5 fw-normal'>Precio: {producto.precio} $</h4>
                   {toCart? 
                     <ItemCount initial={1} stock={producto.stock} onAdd={onAdd}></ItemCount>
                     :
