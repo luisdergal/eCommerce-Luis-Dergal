6df07d38946637ab8ed2aa919265637af14a359a
luisdergal <luisandresdergal@gmail.com>
2022-08-25 03:01:39 -0500

    todas las imagenes a web

--- src/components/CartWidget/Cartwidget.jsx
+++ src/components/CartWidget/Cartwidget.jsx
@@ -3,7 +3,7 @@ const Cartwidget = () => {
 
 
   return (<>
-        <img className='iconoCart' src="../../../assets/images/carritoCompras.svg" alt="" />
+        <img className='iconoCart' src="https://images2.imgbox.com/6d/37/83h0KkmX_o.png" alt="" />
         </>       
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -16,7 +16,7 @@ const NavBar = () => {
             <div className="container-fluid">
 
                 <NavLink to='/'>
-                <span className="navbar-brand ms-4" id='containerLogo'><img src="../../../assets/images/losGamaLogo.svg" id='logoGU' alt="LogoMarca" /></span>
+                <span className="navbar-brand ms-4" id='containerLogo'><img src="https://images2.imgbox.com/d7/b6/MqvMfm7H_o.png" id='logoGU' alt="LogoMarca" /></span>
                 </NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -25,7 +25,7 @@ const NavBar = () => {
                 <ul className="navbar-nav m-auto mb-2 mb-lg-0 navLinks">
                     <li className="nav-item me-2">
                     <NavLink className='nav-link' to='/categoria/Tarjetas_graficas'>Tarjetas gráficas</NavLink> 
-                        <img className='iconos' src="../../../assets/images/tarjeta-grafica.svg" alt="" />
+                        <img className='iconos' src="https://images2.imgbox.com/a4/b9/d3bBMZMJ_o.png" alt="" />
                     </li>
                     <li className="nav-item me-2">
                     <NavLink className='nav-link' to='/categoria/Procesadores'>Procesadores</NavLink> 
@@ -33,15 +33,15 @@ const NavBar = () => {
                     </li>
                     <li className="nav-item me-2">
                     <NavLink className='nav-link' to='/categoria/Motherboards'>Motherboards</NavLink> 
-                    <img className='iconos' src="../../../assets/images/motherboard.svg" alt="" />
+                    <img className='iconos' src="https://images2.imgbox.com/8e/4c/MaHv6sUL_o.png" alt="" />
                     </li>
                     <li className="nav-item me-2">
                     <NavLink className='nav-link' to='/categoria/Fuentes_de_poder'>Fuentes de poder</NavLink> 
-                    <img className='iconos' src="../../../assets/images/power-supply.svg" alt="" />
+                    <img className='iconos' src="https://images2.imgbox.com/6e/37/nwRcnNEf_o.png" alt="" />
                     </li>
                     <li className="nav-item me-2">
                     <NavLink className='nav-link' to='/categoria/Gabinetes'>Gabinetes</NavLink> 
-                    <img className='iconos' src="../../../assets/images/tower-pc.svg" alt="" />
+                    <img className='iconos' src="https://images2.imgbox.com/0e/1a/FziJzcWo_o.png" alt="" />
                     </li>
                     <li className="nav-item me-2">
                     <NavLink className='nav-link' to='/categoria/Memoria_Ram'>Memoria Ram</NavLink> 
@@ -49,7 +49,7 @@ const NavBar = () => {
                     </li>
                     <li className="nav-item me-2">
                     <NavLink className='nav-link' to='/categoria/Almacenamiento'>Almacenamiento</NavLink> 
-                    <img className='iconos' src="../../../assets/images/ssd-drive.svg" alt="" />
+                    <img className='iconos' src="https://images2.imgbox.com/0e/58/z1LNuUN6_o.png" alt="" />
                     </li>
                     <li className="nav-item me-2">
                     <NavLink className='nav-link' to='/categoria/Monitores'>Monitores</NavLink> 
@@ -57,7 +57,7 @@ const NavBar = () => {
                     </li>
                     <li className="nav-item me-2">
                     <NavLink className='nav-link' to='/categoria/Refrigeracion'>Refrigeración</NavLink> 
-                    <img className='iconos' src="../../../assets/images/cooler.svg" alt="" />
+                    <img className='iconos' src="https://images2.imgbox.com/0d/a1/6oehEI1H_o.png" alt="" />
                     </li>
                     <li className="nav-item me-2">
                     <NavLink className='nav-link' to='/categoria/Perifericos'>Periféricos</NavLink> 
